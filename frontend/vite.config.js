1f40fd23f228d9bcf3ef4898748b32cd58cb55b1
Evgeny Zaryanov <eugenezaryanov@yandex.ru>
2023-11-16 01:00:46 +0300

    new

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/react-mesto-auth/',
-  root: './',
-  build: {
-    outDir: 'dist'
-  },
-  publicDir: 'assets',
+  base: '/homepage/',
   plugins: [react()]
 });
