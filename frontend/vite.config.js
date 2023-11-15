2212de95fb446bb78ce8d5589ea1274999e01fe9
Evgeny Zaryanov <eugenezaryanov@yandex.ru>
2023-11-16 00:32:23 +0300

    changes 4.0

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/react-mesto-auth/',
+  base: '/dist/',
   plugins: [react()]
 });
+
+// eslint-disable-next-line no-undef
+module.exports = {
+  root: './',
+  build: {
+    outDir: 'dist'
+  },
+  publicDir: 'assets'
+};
